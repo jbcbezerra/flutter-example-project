105b9cf0732824241c71867f5fac06c0d17b10b6
jbcbezerra <candido.bezerra.coding@gmail.com>
2023-12-25 20:05:11 +0100

    [data][test]: write first model tests

--- test/lib/src/authentication/data/models/user_model_test.dart
+++ test/lib/src/authentication/data/models/user_model_test.dart
@@ -21,7 +21,53 @@ void main() {
     test("should return a [UserModel] with the right data", () {
       // Act
       final result = UserModel.fromMap(tMap);
+
+      // Assert
       expect(result, equals(tModel));
     });
   });
+
+  group('fromJson', () {
+    test("should return a [UserModel] with the right data", () {
+      // Act
+      final result = UserModel.fromJson(tJson);
+
+      // Assert
+      expect(result, tModel);
+    });
+  });
+
+  group('toMap', () {
+    test("should return a [Map] with the right data", () {
+      // Act
+      final result = tModel.toMap();
+
+      // Assert
+      expect(result, equals(tMap));
+    });
+  });
+
+  group('toJson', () {
+    test("should return a [JSON] String with the right data", () {
+      // Act
+      final result = tModel.toJson();
+
+      // Assert
+      expect(result, equals(normalizeString(tJson)));
+    });
+  });
+
+  group('copyWith', () {
+    test("should return a [UserModel] with different data", () {
+      // Act
+      final result = tModel.copyWith(name: "Paul");
+
+      // Assert
+      expect(result.name, equals("Paul"));
+    });
+  });
+}
+
+String normalizeString(String input) {
+  return input.replaceAll(RegExp(r'\s'), '');
 }
