b643f90776350bfed3c7286b03b770285364f474
jbcbezerra <candido.bezerra.coding@gmail.com>
2023-12-25 16:42:04 +0100

    test: create first usecase test

--- test/lib/src/authentication/domain/usecases/create_user_test.dart
+++ test/lib/src/authentication/domain/usecases/create_user_test.dart
@@ -1,5 +1,45 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:tdd_tutorial/src/authentication/domain/repositories/authentication_repository.dart';
+import 'package:tdd_tutorial/src/authentication/domain/usecases/create_user.dart';
 
 // create mockversion of dependencies
 class MockAuthRepo extends Mock implements AuthenticationRepository {}
+
+void main() {
+  late AuthenticationRepository repository;
+  late CreateUser usecase;
+
+  setUp(() {
+    repository = MockAuthRepo();
+    usecase = CreateUser(repository);
+  });
+
+  const params = CreateUserParams(
+      createdAt: '_empty.string',
+      name: '_empty.string',
+      avatar: '_empty.string');
+
+  test("should call the [AuthRepo.createUser]", () async {
+    // Arrange
+    when(() => repository.createUser(
+            createdAt: any(named: 'createdAt'),
+            name: any(named: 'name'),
+            avatar: any(named: 'avatar')))
+        .thenAnswer((_) async => const Right(null));
+
+    // Act
+    final result = await usecase.call(params);
+
+    // Assert
+    expect(result, equals(const Right(null))); // verify return
+    verify(() => repository.createUser(
+        // verify that function was called once
+        createdAt: params.createdAt,
+        name: params.name,
+        avatar: params.avatar)).called(1);
+    verifyNoMoreInteractions(
+        repository); // verify no more interaction with repository after previous
+  });
+}
