850ceb4be414c4ec7cd6650c28e681704041bcf7
jbcbezerra <candido.bezerra.coding@gmail.com>
2023-12-25 15:47:02 +0100

    implement usecase abstraction

--- lib/src/authentication/domain/usecases/create_user.dart
+++ lib/src/authentication/domain/usecases/create_user.dart
@@ -1,15 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:tdd_tutorial/core/usecase/usecase.dart';
 import 'package:tdd_tutorial/src/authentication/domain/repositories/authentication_repository.dart';
 
 import '../../../../core/utilities/typedef.dart';
 
-class CreateUser {
+class CreateUser extends UsecaseWithParameters<void, CreateUserParams> {
   final AuthenticationRepository _repository;
 
   const CreateUser(this._repository);
 
-  ResultVoid createUser(
-          {required String createdAt,
-          required String name,
-          required String avatar}) async =>
-      _repository.createUser(createdAt: createdAt, name: name, avatar: avatar);
+  @override
+  ResultVoid call(CreateUserParams params) async {
+    return _repository.createUser(
+        createdAt: params.createdAt, name: params.name, avatar: params.avatar);
+  }
+}
+
+class CreateUserParams extends Equatable {
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  const CreateUserParams(
+      {required this.createdAt, required this.name, required this.avatar});
+
+  @override
+  List<Object?> get props => [createdAt, name, avatar];
 }
---
+++ lib/src/authentication/domain/usecases/get_users.dart
@@ -0,0 +1,15 @@
+import 'package:tdd_tutorial/core/usecase/usecase.dart';
+import 'package:tdd_tutorial/core/utilities/typedef.dart';
+import 'package:tdd_tutorial/src/authentication/domain/entities/user.dart';
+import 'package:tdd_tutorial/src/authentication/domain/repositories/authentication_repository.dart';
+
+class GetUsers extends UsecaseWithoutParameters<List<User>> {
+  final AuthenticationRepository _repository;
+
+  const GetUsers(this._repository);
+
+  @override
+  ResultFuture<List<User>> call() {
+    return _repository.getAllUsers();
+  }
+}
