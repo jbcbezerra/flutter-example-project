8185286f43280fb59021b8eb4e05ee8d20c3e6f2
jbcbezerra <candido.bezerra.coding@gmail.com>
2023-12-27 14:26:42 +0100

    [presentation][bloc]: create states for context

--- lib/src/authentication/presentation/bloc/auth_state.dart
+++ lib/src/authentication/presentation/bloc/auth_state.dart
@@ -2,9 +2,44 @@ part of 'auth_bloc.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
+
+  @override
+  List<Object> get props => [];
 }
 
 class AuthInitial extends AuthState {
+  const AuthInitial();
+
   @override
   List<Object> get props => [];
+}
+
+class CreatingUser extends AuthState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthState {
+  final List<User> users;
+
+  const UsersLoaded(this.users);
+
+  @override
+  List<Object> get props => users.map((u) => u.id).toList();
+}
+
+class AuthError extends AuthState {
+  final String msg;
+
+  const AuthError(this.msg);
+
+  @override
+  List<Object> get props => [msg];
 }
